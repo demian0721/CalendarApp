6cbe5b9fd6de2f0d62cf4104843215670a777d21
demian0721 <83893777+demian0721@users.noreply.github.com>
2022-09-29 18:30:24 +0900

    fix: 빌드를 위한 임시 주석처리

--- app/src/main/java/ui/calendar/CalendarAdapter.kt
+++ app/src/main/java/ui/calendar/CalendarAdapter.kt
@@ -1,4 +1,13 @@
-package ui.calendar
-
-class CalendarAdapter {
-}
+//package ui.calendar
+//
+//import androidx.fragment.app.Fragment
+//import androidx.fragment.app.FragmentActivity
+//import androidx.viewpager2.adapter.FragmentStateAdapter
+//
+//class CalendarAdapter(fragmentActivity:FragmentActivity):FragmentStateAdapter(fragmentActivity) {
+//    var fragments : ArrayList<Fragment> = ArrayList()
+//
+//    override fun getItemCount(): Int {
+//        return fragments.size
+//    }
+//}
