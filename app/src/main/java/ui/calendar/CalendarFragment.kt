7bc66883b58200f3e1fbff3dd41940aba9948a9b
demian0721 <83893777+demian0721@users.noreply.github.com>
2022-09-30 18:12:49 +0900

    feat: Adapter, Util, Fragment 로직 구현

--- app/src/main/java/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/ui/calendar/CalendarFragment.kt
@@ -5,23 +5,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.calendar.app.R
 import com.calendar.app.databinding.FragmentCalendarBinding
+import org.joda.time.DateTime
+import ui.calendar.CalendarUtils.Companion.getMonthList
 
-class CalendarFragment:Fragment() {
+class CalendarFragment : Fragment() {
+
+    private var millis: Long = 0L
     private lateinit var binding: FragmentCalendarBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            millis = it.getLong(MILLIS)
+        }
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding=FragmentCalendarBinding.inflate(inflater,container,false)
+        binding = FragmentCalendarBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.lifecycleOwner=viewLifecycleOwner
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    companion object {
+
+        private const val MILLIS = "MILLIS"
+
+        fun newInstance(millis: Long) = CalendarFragment().apply {
+            arguments = Bundle().apply {
+                putLong(MILLIS, millis)
+            }
+        }
     }
 }
